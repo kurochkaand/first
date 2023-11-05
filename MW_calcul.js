d792ec1c697a79b362b18d32631febabf795c720
kurochkaand <kurochka.and@gmail.com>
2023-11-05 18:33:39 +0100

    logic of keyboard

--- MW_calcul.js
+++ MW_calcul.js
@@ -143,7 +143,7 @@ function mass_counter(arr) {
       if (ptable.hasOwnProperty(ele)) {
         result += ptable[ele];
       } else {
-        document.getElementById("result").innerHTML = `unrecognized "${ele}" !`;
+        document.getElementById("result").value = `unrecognized "${ele}" !`;
         return 0;
       }
     } else if (!isNaN(parseInt(arr[i])) && i == 0) {
@@ -174,13 +174,150 @@ document.getElementById("sbm").onclick = function (event) {
     g_result += mass_counter(tmp_a3.slice(1)); //counted after bracet omiting first index
   }
   if (!isNaN(g_result) && g_result > 0) {
-    document.getElementById("result").innerHTML = g_result.toFixed(1);
+    document.getElementById("result").value = g_result.toFixed(1);
   } else {
-    document.getElementById("result").innerHTML = "Wrong input!";
+    document.getElementById("result").value = "Wrong input!";
   }
 };
 
 // INteractive periodic table
-const elements = document.querySelectorAll(".element");
+var numToName = {
+  1: "H",
+  2: "He",
+  3: "Li",
+  4: "Be",
+  5: "B",
+  6: "C",
+  7: "N",
+  8: "O",
+  9: "F",
+  10: "Ne",
+  11: "Na",
+  12: "Mg",
+  13: "Al",
+  14: "Si",
+  15: "P",
+  16: "S",
+  17: "Cl",
+  18: "Ar",
+  19: "K",
+  20: "Ca",
+  21: "Sc",
+  22: "Ti",
+  23: "V",
+  24: "Cr",
+  25: "Mn",
+  26: "Fe",
+  27: "Co",
+  28: "Ni",
+  29: "Cu",
+  30: "Zn",
+  31: "Ga",
+  32: "Ge",
+  33: "As",
+  34: "Se",
+  35: "Br",
+  36: "Kr",
+  37: "Rb",
+  38: "Sr",
+  39: "Y",
+  40: "Zr",
+  41: "Nb",
+  42: "Mo",
+  43: "Tc",
+  44: "Ru",
+  45: "Rh",
+  46: "Pd",
+  47: "Ag",
+  48: "Cd",
+  49: "In",
+  50: "Sn",
+  51: "Sb",
+  52: "Te",
+  53: "I",
+  54: "Xe",
+  55: "Cs",
+  56: "Ba",
+  57: "La",
+  58: "Ce",
+  59: "Pr",
+  60: "Nd",
+  61: "Pm",
+  62: "Sm",
+  63: "Eu",
+  64: "Gd",
+  65: "Tb",
+  66: "Dy",
+  67: "Ho",
+  68: "Er",
+  69: "Tm",
+  70: "Yb",
+  71: "Lu",
+  72: "Hf",
+  73: "Ta",
+  74: "W",
+  75: "Re",
+  76: "Os",
+  77: "Ir",
+  78: "Pt",
+  79: "Au",
+  80: "Hg",
+  81: "Tl",
+  82: "Pb",
+  83: "Bi",
+  84: "Po",
+  85: "At",
+  86: "Rn",
+  87: "Fr",
+  88: "Ra",
+  89: "Ac",
+  90: "Th",
+  91: "Pa",
+  92: "U",
+  93: "Np",
+  94: "Pu",
+  95: "Am",
+  96: "Cm",
+  97: "Bk",
+  98: "Cf",
+  99: "Es",
+  100: "Fm",
+  101: "Md",
+  102: "No",
+  103: "Lr",
+  104: "Rf",
+  105: "Db",
+  106: "Sg",
+  107: "Bh",
+  108: "Hs",
+  109: "Mt",
+  110: "Ds",
+  111: "Rg",
+  112: "Cn",
+  113: "Nh",
+  114: "Fl",
+  115: "Mc",
+  116: "Lv",
+  117: "Ts",
+  118: "Og",
+  201: "0",
+  202: "1",
+  203: "2",
+  204: "3",
+  205: "4",
+  206: "5",
+  207: "6",
+  208: "7",
+  209: "8",
+  2010: "9",
+  2011: "(",
+  2012: ")",
+};
 
-function eleClicked(ind) {}
+const elements = document.querySelectorAll(".element");
+elements.forEach((cell) => cell.addEventListener("click", eleClicked));
+
+function eleClicked() {
+  let ind = this.getAttribute("index");
+  document.getElementById("formula").value += numToName[ind];
+}
