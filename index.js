9b628578dd39c32c049759a9c5e30950697f983c
kurochkaand <kurochka.and@gmail.com>
2023-11-12 00:24:35 +0100

    conc2 calculator logic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById("conc").onclick = function (event) {
   let ab = document.getElementById("ab").value;
   let ec = document.getElementById("ec").value;
   let pl = document.getElementById("pl").value;
-  let concentration1 = document.getElementById("concentration1").value;
+  let con1 = document.getElementById("concentration1").value;
   let c1_unit = document.getElementById("c1_unit").value;
   let mult = -2;
   if (c1_unit === "nM") {
@@ -15,17 +15,17 @@ document.getElementById("conc").onclick = function (event) {
   }
 
   if (ab === "") {
-    ab = (ec * concentration1 * pl) / mult;
-    document.getElementById("ab").value = ab;
+    ab = (ec * con1 * pl) / mult;
+    document.getElementById("ab").value = ab.toFixed(3);
   } else if (ec === "") {
-    ec = (ab * pl * mult) / concentration1;
-    document.getElementById("ec").value = ec;
+    ec = (ab * pl * mult) / con1;
+    document.getElementById("ec").value = ec.toFixed(3);
   } else if (pl === "") {
-    pl = (ab * mult) / (concentration1 * ec);
-    document.getElementById("pl").value = pl;
+    pl = (ab * mult) / (con1 * ec);
+    document.getElementById("pl").value = pl.toFixed(3);
   } else {
-    concentration1 = (ab * pl * mult) / ec;
-    document.getElementById("concentration1").value = concentration1;
+    con1 = (ab * pl * mult) / ec;
+    document.getElementById("concentration1").value = con1.toFixed(3);
   }
 };
 // Reset button of the first calculator
@@ -44,36 +44,60 @@ document.getElementById("reset1").onclick = function (event) {
 document.getElementById("calculate2").onclick = function (event) {
   let mass = document.getElementById("mass").value;
   let m_unit = document.getElementById("m_unit").value;
+  let mass_mult;
   if (m_unit == "mg") {
-    mass = mass / 1e3;
+    mass_mult = 1e-3;
+  } else if (m_unit == "g") {
+    mass_mult = 1;
   } else if (m_unit == "kg") {
-    mass = mass * 1e3;
+    mass_mult = 1e3;
   }
+
   let vol = document.getElementById("vol").value;
   let v_unit = document.getElementById("v_unit").value;
+  let vol_mult;
   if (v_unit == "μL") {
-    vol = vol / 1e6;
+    vol_mult = 1e6;
   } else if (v_unit == "mL") {
-    vol = vol / 1e3;
+    vol_mult = 1e3;
+  } else if (v_unit == "L") {
+    vol_mult = 1;
   }
+
   let mw = document.getElementById("mw").value;
   let mw_unit = document.getElementById("mw_unit").value;
+  let mw_mult;
   if (mw_unit == "kDa") {
-    mw = mw * 1e3;
+    mw_mult = 1e3;
+  } else if (mw_unit == "Da") {
+    mw_mult = 1;
   }
+
   let prec_2 = document.getElementById("prec_2").value;
-  //Equation is here
-  let result2 = mass / (mw * vol);
-  //Units of second result
+
+  let con2 = document.getElementById("concentration2").value;
   let c2_unit = document.getElementById("c2_unit").value;
+  let con_mult2;
   if (c2_unit == "nM") {
-    result2 *= 1e9;
+    con_mult2 = 1e9;
   } else if (c2_unit == "μM") {
-    result2 *= 1e6;
+    con_mult2 = 1e6;
   } else if (c2_unit == "mM") {
-    result2 *= 1e3;
+    con_mult2 = 1e3;
   }
-  document.getElementById("result2").innerHTML = result2.toFixed(prec_2);
+  if (mass === "") {
+    mass = vol * con2 * mw;
+    document.getElementById("mass").value = mass.toFixed(3);
+  } else if (vol === "") {
+    vol = mass / (con2 * mw);
+    document.getElementById("vol").value = vol.toFixed(3);
+  } else if (mw === "") {
+    mw = mass / (vol * con2);
+    document.getElementById("mw").value = mw.toFixed(3);
+  } else {
+    con2 = mass / (vol * mw);
+    document.getElementById("concentration2").value = con2.toFixed(prec_2);
+  }
 };
 
 // Reset button of the second calculator
